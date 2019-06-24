aab1331c9ec1719d105d51dc2d66da9f086c9247
Leo Botinelly <lmbs001@bucknell.edu>
2019-06-23 23:39:37 -0400

    Flair

--- Sample/SimpleWebApp/Startup.cs
+++ Sample/SimpleWebApp/Startup.cs
@@ -8,25 +8,20 @@ namespace SimpleWebApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddZenWeb();
+            services.AddZenWeb();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app
-                .UseZenWeb(options => { options.Options.UseHtml5 = true; }, env);
+            app.UseZenWeb(options => { options.Options.UseHtml5 = true; }, env);
         }
     }
 }
