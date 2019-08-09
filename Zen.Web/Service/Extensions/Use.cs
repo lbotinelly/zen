60cf5429f5dc840d5d7655fd6eb994ef99808f07
Leo Botinelly <lmbs001@bucknell.edu>
2019-08-09 14:23:43 -0400

    wwwroot mapping

--- Zen.Web/Service/Extensions/Use.cs
+++ Zen.Web/Service/Extensions/Use.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
@@ -20,7 +22,9 @@ namespace Zen.Web.Service.Extensions
             var builder = new ZenWebBuilder(app, options);
 
             app.UseDefaultFiles();
-            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")), RequestPath = "" });
+
 
             app
                 .UseHttpsRedirection()
