b488ee9e3cca740d850627d2f5c6bb4e1ab33a95
Leo Botinelly <lmbs001@bucknell.edu>
2019-08-19 08:42:40 -0400

    Strong-typed Configuration

---
+++ Zen.App/Model/Application.SettingsDescriptor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Zen.App.Orchestrator.Model
+{
+    public partial class Application
+    {
+        public class SettingsDescriptor
+        {
+            public class GroupDescriptor
+            {
+                public string Code { get; set; }
+                public string Name { get; set; }
+                public List<string> Permissions { get; set; }
+                public List<string> Members { get; set; }
+                public bool IsHost { get; set; } = false;
+            }
+
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public string Locator { get; set; }
+            public List<GroupDescriptor> Groups { get; set; }
+        }
+    }
+}
