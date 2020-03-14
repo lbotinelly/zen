6ae581d5446ff4189707fb5cd5a95344d6d2c0fd
Leo Botinelly <lmbs001@bucknell.edu>
2020-03-14 01:49:45 -0400

    REST example

---
+++ Samples/Sample02-REST/Controller/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample02_REST.Model;
+using Zen.Web.Data.Controller;
+
+namespace Sample02_REST.Controller
+{
+    [Route("api/people")]
+    public class PersonController : DataController<Person>
+    {
+    }
+}
