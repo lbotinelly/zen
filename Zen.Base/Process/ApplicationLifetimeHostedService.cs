7a288cc1101d09a4874fe9deee391f4b344d02b3
Leo Botinelly <lmbs001@bucknell.edu>
2020-03-20 14:29:56 -0400

    Cleanup

--- Zen.Base/Process/ApplicationLifetimeHostedService.cs
+++ Zen.Base/Process/ApplicationLifetimeHostedService.cs
@@ -6,51 +6,45 @@ using Zen.Base.Module.Log;
 
 namespace Zen.Base.Process
 {
-    namespace Core.Service.Sample
+    // https://dejanstojanovic.net/aspnet/2018/june/clean-service-stop-on-linux-with-net-core-21/
+    public class ApplicationLifetimeHostedService : IHostedService
     {
-        // https://dejanstojanovic.net/aspnet/2018/june/clean-service-stop-on-linux-with-net-core-21/
-        public class ApplicationLifetimeHostedService : IHostedService
+        private readonly IHostApplicationLifetime appLifetime;
+        private IConfiguration configuration;
+        private IHostEnvironment environment;
+
+        public ApplicationLifetimeHostedService(IConfiguration configuration, IHostEnvironment environment, IHostApplicationLifetime appLifetime)
         {
-            private readonly IHostApplicationLifetime appLifetime;
-            private IConfiguration configuration;
-            private IHostEnvironment environment;
-
-            public ApplicationLifetimeHostedService(
-                IConfiguration configuration,
-                IHostEnvironment environment,
-                IHostApplicationLifetime appLifetime)
-            {
-                this.configuration = configuration;
-                this.appLifetime = appLifetime;
-                this.environment = environment;
-            }
-
-            public Task StartAsync(CancellationToken cancellationToken)
-            {
-                Log.KeyValuePair(Host.ApplicationAssemblyName, "StartAsync", Message.EContentType.StartupSequence);
-
-                appLifetime.ApplicationStarted.Register(OnStarted);
-                appLifetime.ApplicationStopping.Register(OnStopping);
-                appLifetime.ApplicationStopped.Register(OnStopped);
-
-                return Task.CompletedTask;
-            }
-
-            public Task StopAsync(CancellationToken cancellationToken)
-            {
-                Log.KeyValuePair(Host.ApplicationAssemblyName, "StopAsync", Message.EContentType.StartupSequence);
-                return Task.CompletedTask;
-            }
-
-            private void OnStarted() { Log.KeyValuePair(Host.ApplicationAssemblyName, "OnStarted", Message.EContentType.StartupSequence); }
-
-            private void OnStopping()
-            {
-                Log.KeyValuePair(Host.ApplicationAssemblyName, "OnStopping", Message.EContentType.ShutdownSequence);
-                appLifetime?.StopApplication();
-            }
-
-            private void OnStopped() { Log.KeyValuePair(Host.ApplicationAssemblyName, "OnStopped", Message.EContentType.ShutdownSequence); }
+            this.configuration = configuration;
+            this.appLifetime = appLifetime;
+            this.environment = environment;
         }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Log.KeyValuePair(Host.ApplicationAssemblyName, "StartAsync", Message.EContentType.StartupSequence);
+
+            appLifetime.ApplicationStarted.Register(OnStarted);
+            appLifetime.ApplicationStopping.Register(OnStopping);
+            appLifetime.ApplicationStopped.Register(OnStopped);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Log.KeyValuePair(Host.ApplicationAssemblyName, "StopAsync", Message.EContentType.StartupSequence);
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted() { Log.KeyValuePair(Host.ApplicationAssemblyName, "OnStarted", Message.EContentType.StartupSequence); }
+
+        private void OnStopping()
+        {
+            Log.KeyValuePair(Host.ApplicationAssemblyName, "OnStopping", Message.EContentType.ShutdownSequence);
+            appLifetime?.StopApplication();
+        }
+
+        private void OnStopped() { Log.KeyValuePair(Host.ApplicationAssemblyName, "OnStopped", Message.EContentType.ShutdownSequence); }
     }
 }
