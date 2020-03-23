4574471a6d7b3949319a3bcee3c1c8de983fb7d7
Leo Botinelly <lmbs001@bucknell.edu>
2020-03-23 15:06:36 -0400

    Create Defaults.cs

---
+++ Zen.Web/Host/Defaults.cs
@@ -0,0 +1,10 @@
+﻿namespace Zen.Web.Host
+{
+    public static class Defaults
+    {
+        public static int WebHttpPort = 5000;
+        public static int WebHttpsPort = 5001;
+        public static string WebQualifiedServerName = null;
+
+    }
+}
