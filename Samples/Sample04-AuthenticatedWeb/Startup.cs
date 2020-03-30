d53b8a3f2edb0e2d5be0e82ded9fa0549679c6a5
Leo Botinelly <lmbs001@bucknell.edu>
2020-03-30 16:29:17 -0400

    Sample04

---
+++ Samples/Sample04-AuthenticatedWeb/Startup.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Zen.Base.Service.Extensions;
+
+namespace Sample04_AuthenticatedWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddZen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseZen();
+        }
+    }
+}
