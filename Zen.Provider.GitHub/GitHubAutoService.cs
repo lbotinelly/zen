2639f001e7b053dc1fb3138dd5b13c170122baff
Leo Botinelly <lmbs001@bucknell.edu>
2020-03-30 16:47:47 -0400

    GitHub provider

---
+++ Zen.Provider.GitHub/GitHubAutoService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Zen.Base.Module.Service;
+using Zen.Provider.GitHub.Authentication;
+
+namespace Zen.Provider.GitHub
+{
+    public class GitHubAutoService : IZenAutoAddService
+    {
+        public void Add(IServiceCollection services)
+        {
+            services.AddAuthenticationProvider();
+        }
+    }
+}
