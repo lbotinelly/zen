e565d4a5049324ee184a1cf76d19b41f4b110fc6
Leo Botinelly <lmbs001@bucknell.edu>
2020-03-30 16:47:20 -0400

    Microsoft provider

---
+++ Zen.Provider.Microsoft/Authentication/AutoSettingsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Zen.Base;
+using Zen.Web.Auth;
+
+namespace Zen.Provider.Microsoft.Authentication
+{
+    public static class MicrosoftAuthentication
+    {
+        internal static IServiceCollection ResolveSettingsPackage(this IServiceCollection services)
+        {
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true);
+
+            Instances.AuthenticationBuilder.AddMicrosoftAccount(options =>
+            {
+                options.ClientId = Configuration.Options["Authentication:Microsoft:ClientId"];
+                options.ClientSecret = Configuration.Options["Authentication:Microsoft:ClientSecret"];
+                options.CallbackPath = "/auth/signin/microsoft";
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Zen.Provider.Microsoft/Service/MicrosoftAutoService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Zen.Base.Common;
+using Zen.Base.Module.Service;
+using Zen.Provider.Microsoft.Authentication;
+
+namespace Zen.Provider.Microsoft.Service
+{
+    [Priority(Level = -99)]
+    public class MicrosoftAutoService : IZenAutoAddService
+    {
+        public void Add(IServiceCollection services)
+        {
+            services.ResolveSettingsPackage();
+        }
+    }
+}
