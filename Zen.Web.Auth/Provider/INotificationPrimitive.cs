4873d5f29ba2fcb05525ace5481df4b35cb5d106
Leo Botinelly <lmbs001@bucknell.edu>
2020-04-02 11:24:33 -0400

    oops.

---
+++ Zen.Web.Auth/Provider/INotificationPrimitive.cs
@@ -0,0 +1,7 @@
+﻿namespace Zen.Web.Auth.Provider {
+    public interface INotificationPrimitive
+    {
+        NotificationOptions options { get; set; }
+        string title { get; set; }
+    }
+}
---
+++ Zen.Web.Auth/Provider/NotificationPrimitive.cs
@@ -0,0 +1,7 @@
+﻿namespace Zen.Web.Auth.Provider {
+    public abstract class NotificationPrimitive : INotificationPrimitive
+    {
+        public NotificationOptions options { get; set; }
+        public string title { get; set; }
+    }
+}
