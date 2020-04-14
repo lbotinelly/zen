4d888154299dc23fa6bfe648ba7b3f4d4a5103da
Leo Botinelly <lmbs001@bucknell.edu>
2020-04-14 10:13:41 -0400

    Initial modification: multiple providers

--- Zen.Web.Auth/Controller/AuthController.cs
+++ Zen.Web.Auth/Controller/AuthController.cs
@@ -1,5 +1,10 @@
 ﻿using System.Collections.Concurrent;
-using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Zen.App.Provider;
 using Zen.Base.Extension;
@@ -13,16 +18,55 @@ namespace Zen.Web.Auth.Controller
     [Route("auth")]
     public class AuthController : ControllerBase
     {
-        [Authorize, HttpGet("signin")]
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet("signin")]
         public object SignIn()
         {
-            var person = App.Current.Orchestrator.SigninPersonByIdentity(User.Identity);
+            var returnUrl = Request.Query["sourceUrl"].FirstOrDefault() ?? Url.Content("~/");
+            var provider = Request.Query["provider"].FirstOrDefault() ?? "Google";
 
-            if (Request.Query.ContainsKey("sourceUrl")) return Redirect(Request.Query["sourceUrl"]);
+            var gatewayUrl = $"gateway?returnUrl={System.Net.WebUtility.UrlEncode(returnUrl)}";
 
-            Base.Current.Log.Add(person.ToJson());
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, gatewayUrl);
+            var challenge = new ChallengeResult(provider, properties);
 
-            return person;
+            return challenge;
+
+        }
+
+
+        [HttpGet("signin/gateway")]
+        public async Task<IActionResult> OnPostConfirmationAsync([FromQuery]string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            // Get the information about the user from the external login provider
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            if (info == null) return Problem("Error loading external login information during confirmation.");
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                var props = new AuthenticationProperties();
+                props.StoreTokens(info.AuthenticationTokens);
+                props.IsPersistent = true;
+
+                await _signInManager.SignInAsync(user, props);
+
+                var person = App.Current.Orchestrator.SigninPersonByIdentity(User.Identity);
+                Base.Current.Log.Add(person.ToJson());
+            }
+
+            return  LocalRedirect(returnUrl);
         }
 
         [HttpGet("signout")]
@@ -30,7 +74,7 @@ namespace Zen.Web.Auth.Controller
         {
             // Call the SignOut endpoint from the API, if in clientmode, or its own.
 
-            Web.Current.Context.Session.Clear();
+            Current.Context.Session.Clear();
 
             var url = App.Current.Orchestrator.GetApiUri() + "/framework/auth/signout";
             return new RedirectResult(url);
@@ -49,9 +93,12 @@ namespace Zen.Web.Auth.Controller
         {
             var tmp = App.Current.Orchestrator.Person?.ToPropertyDictionary() ?? new ConcurrentDictionary<string, object>();
 
-            tmp["IsAuthenticated"] = App.Current.Orchestrator.Person!= null;
+            tmp["IsAuthenticated"] = App.Current.Orchestrator.Person != null;
 
             return tmp;
         }
+
+        [HttpGet("providers")]
+        public IEnumerable<AuthenticationScheme> GetProviders() => _signInManager.GetExternalAuthenticationSchemesAsync().Result;
     }
 }
