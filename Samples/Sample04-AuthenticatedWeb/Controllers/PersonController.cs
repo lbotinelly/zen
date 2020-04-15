956f9862d7e57c9016d275a18efbd78135910403
Leo Botinelly <lmbs001@bucknell.edu>
2020-04-15 10:21:58 -0400

    Sample REST controllers

---
+++ Samples/Sample04-AuthenticatedWeb/Controllers/PersonController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zen.App.Model.Core;
+using Zen.Web.Data.Controller;
+
+namespace Sample04_AuthenticatedWeb.Controllers
+{
+    [Route("data/auth/person")]
+    public class PersonController : DataController<Person> { }
+}
---
+++ Samples/Sample04-AuthenticatedWeb/Controllers/ProviderIdentityUserController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zen.Web.Auth.Model;
+using Zen.Web.Data.Controller;
+
+namespace Sample04_AuthenticatedWeb.Controllers {
+    [Route("data/auth/identity")]
+    public class ProviderIdentityUserController : DataController<Identity> { }
+}
