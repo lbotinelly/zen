749b1458cf314aa13923d7138d1aef682271f00a
Leo Botinelly <lmbs001@bucknell.edu>
2020-04-17 17:22:58 -0400

    Update RedisCacheProvider.cs

--- Zen.Module.Cache.Redis/RedisCacheProvider.cs
+++ Zen.Module.Cache.Redis/RedisCacheProvider.cs
@@ -11,77 +11,9 @@ using Zen.Base.Module.Log;
 namespace Zen.Module.Cache.Redis
 {
     [Priority(Level = -1)]
-    public class RedisCacheProvider : ICacheProvider
+    public class RedisCacheProvider : PrimitiveCacheProvider, IZenProvider
     {
-        private static ConnectionMultiplexer _redis;
-
-        private static string _currentServer = "none";
-        public Dictionary<string, ICacheConfiguration> EnvironmentConfiguration { get; set; }
-
-        public string this[string key, string oSet = null, int cacheTimeOutSeconds = 600]
-        {
-            get
-            {
-                if (OperationalStatus != EOperationalStatus.Operational) return null;
-
-                try
-                {
-                    var db = _redis.GetDatabase(DatabaseIndex);
-                    var res = db.StringGet(key);
-                    return res;
-                } catch (Exception e)
-                {
-                    Current.Log.Add(e);
-                    return null;
-
-                    //OperationalStatus = EOperationalStatus.Error;
-                    //throw;
-                }
-            }
-            set
-            {
-                if (OperationalStatus != EOperationalStatus.Operational) return;
-
-                try
-                {
-                    var db = _redis.GetDatabase(DatabaseIndex);
-
-                    if (cacheTimeOutSeconds == 0) db.StringSet(key, value);
-                    else db.StringSet(key, value, TimeSpan.FromSeconds(cacheTimeOutSeconds));
-
-                    //if (oSet!= null)
-                    //    db.SetAdd(oSet, value);
-                } catch (Exception e)
-                {
-                    Current.Log.Add(e);
-
-                    //OperationalStatus = EOperationalStatus.Error;
-                    //throw;
-                }
-            }
-        }
-
-        public string ServerName { get; private set; }
-
-        public EOperationalStatus OperationalStatus { get; set; }
-
-        public bool Contains(string key)
-        {
-            if (OperationalStatus != EOperationalStatus.Operational) return false;
-
-            try
-            {
-                var db = _redis.GetDatabase(DatabaseIndex);
-                var res = db.KeyExists(key);
-                return res;
-            } catch (Exception)
-            {
-                OperationalStatus = EOperationalStatus.Error;
-                throw;
-            }
-        }
-
-        public IEnumerable<string> GetAll(string oNamespace)
+        public override IEnumerable<string> GetKeys(string oNamespace = null)
         {
             if (OperationalStatus != EOperationalStatus.Operational) return null;
 
@@ -90,76 +22,116 @@ namespace Zen.Module.Cache.Redis
                 var db = _redis.GetDatabase(DatabaseIndex);
                 var conn = _redis.GetEndPoints()[0];
                 var svr = _redis.GetServer(conn);
-                var keys = svr.Keys(pattern: oNamespace + "*").ToList();
+                var keys = svr.Keys(pattern: "*").ToList();
 
                 var ret = keys.Select(a => db.StringGet(a).ToString()).ToList();
                 return ret;
+            }
+            catch (Exception)
+            {
+                OperationalStatus = EOperationalStatus.Error;
+                throw;
+            }
 
-                //return db.SetMembers(oNamespace).Select(i => i.ToString()).ToList();
-            } catch (Exception)
+        }
+
+        public override bool Contains(string key)
+        {
+            if (OperationalStatus != EOperationalStatus.Operational) return false;
+
+            try
+            {
+                var db = _redis.GetDatabase(DatabaseIndex);
+                var res = db.KeyExists(key);
+                return res;
+            }
+            catch (Exception)
             {
                 OperationalStatus = EOperationalStatus.Error;
                 throw;
             }
         }
 
-        public T GetSingleton<T>(string fullName = null)
-        {
-            var n = (fullName ?? typeof(T).FullName) + ":s";
-            var c = this[n];
-
-            return c == null ? default(T) : c.FromJson<T>();
-        }
-
-        public void Remove(string key, string oSet = null)
+        public override void SetNative(string key, string serializedModel)
         {
             if (OperationalStatus != EOperationalStatus.Operational) return;
 
             try
             {
                 var db = _redis.GetDatabase(DatabaseIndex);
-                //if (oSet!= null)
-                //db.SetRemove(oSet, this[key]);
 
-                db.KeyDelete(key);
-            } catch { }
-        }
-
-        public void RemoveAll(string oSet = null)
-        {
-            if (OperationalStatus != EOperationalStatus.Operational) return;
-
-            foreach (var endPoint in _redis.GetEndPoints())
+                //if (cacheTimeOutSeconds == 0) db.StringSet(key, serializedModel);
+                //else 
+                db.StringSet(key, serializedModel, TimeSpan.FromSeconds(cacheTimeOutSeconds));
+            }
+            catch (Exception e)
             {
-                var server = _redis.GetServer(endPoint);
-
-                var db = _redis.GetDatabase(DatabaseIndex);
-
-                var keys = server.Keys(pattern: "*", database: DatabaseIndex).ToList();
-
-                Current.Log.Add("REDIS: Removing {0} keys from database {1}".format(keys.Count, DatabaseIndex), Message.EContentType.Maintenance);
-
-                foreach (var key in keys) db.KeyDelete(key);
-
-                //server.FlushDatabase(DatabaseIndex);
+                Current.Log.Add(e);
             }
         }
 
-        public void SetSingleton(object value, string fullName = null)
+        public override string GetNative(string key)
+        {
+            if (OperationalStatus != EOperationalStatus.Operational) return null;
+
+            try
+            {
+                var db = _redis.GetDatabase(DatabaseIndex);
+                var res = db.StringGet(key);
+                return res;
+            }
+            catch (Exception e)
+            {
+                Current.Log.Add(e);
+                return null;
+            }
+        }
+
+        public override void Remove(string fullKey)
+        {
+            if (fullKey == null) throw new ArgumentNullException(nameof(fullKey));
+            if (OperationalStatus != EOperationalStatus.Operational) return;
+
+            try
+            {
+                _redis.GetDatabase(DatabaseIndex).KeyDelete(fullKey);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        public override void RemoveAll()
         {
             if (OperationalStatus != EOperationalStatus.Operational) return;
 
-            var n = (fullName ?? value.GetType().FullName) + ":s";
-            this[n] = value.ToJson();
+            try
+            {
+                foreach (var endPoint in _redis.GetEndPoints())
+                {
+                    var server = _redis.GetServer(endPoint);
+
+                    var db = _redis.GetDatabase(DatabaseIndex);
+
+                    var keys = server.Keys(pattern: "*", database: DatabaseIndex).ToList();
+
+                    Current.Log.Add("REDIS: Removing {0} keys from database {1}".format(keys.Count, DatabaseIndex), Message.EContentType.Maintenance);
+
+                    foreach (var key in keys) db.KeyDelete(key);
+                }
+            }
+            catch (Exception e)
+            {
+                Current.Log.Add($"REDIS: {e.Message}", Message.EContentType.Exception);
+            }
         }
+
+        public override string Name { get; } = "Redis";
 
         #region driver-specific implementation
 
-        private static int DatabaseIndex { get; set; } = -1;
-
-        
-
-        public void Initialize()
+        public override void Initialize()
         {
             //In the case nothing is defined, a standard environment setup is provided.
             if (EnvironmentConfiguration == null)
@@ -175,7 +147,14 @@ namespace Zen.Module.Cache.Redis
             Connect();
         }
 
-        public void Shutdown() { }
+        private static ConnectionMultiplexer _redis;
+
+        private static string _currentServer = "none";
+
+        private static int DatabaseIndex { get; set; } = -1;
+        internal string ServerName { get; private set; }
+
+        private int cacheTimeOutSeconds = 600;
 
         public void Connect()
         {
@@ -184,13 +163,18 @@ namespace Zen.Module.Cache.Redis
                 ServerName = _currentServer.Split(',')[0];
 
                 //The connection string may be encrypted. Try to decrypt it, but ignore if it fails.
-                try { _currentServer = Current.Encryption.Decrypt(_currentServer); } catch { }
+                try
+                {
+                    _currentServer = Current.Encryption.Decrypt(_currentServer);
+                }
+                catch { }
 
                 Events.AddLog("Redis server", _currentServer.SafeArray("password"));
 
                 _redis = ConnectionMultiplexer.Connect(_currentServer);
                 OperationalStatus = EOperationalStatus.Operational;
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 OperationalStatus = EOperationalStatus.NonOperational;
                 Events.AddLog("REDIS server", "Unavailable - running on direct database mode");
