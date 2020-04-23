2f3c5d9939465162a5662a43fc201602710b3249
Leo Botinelly <lmbs001@bucknell.edu>
2020-04-23 16:13:37 -0400

    Update IConnectionBundleProvider.cs

--- Zen.Base/Module/Data/Connection/IConnectionBundleProvider.cs
+++ Zen.Base/Module/Data/Connection/IConnectionBundleProvider.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using Zen.Base.Common;
 
 namespace Zen.Base.Module.Data.Connection
 {
-    public interface IConnectionBundleProvider
+    public interface IConnectionBundleProvider: IZenProvider
     {
         Type DefaultBundleType { get; set; }
         Dictionary<string, IConnectionBundle> Bundles { get; set; }
