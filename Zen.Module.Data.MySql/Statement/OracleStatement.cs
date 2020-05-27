2170628f335c23f80c18fcec8b2461ff699609e2
Leo Botinelly <lmbs001@bucknell.edu>
2020-05-27 13:20:55 -0400

    Delete OracleStatement.cs

--- Zen.Module.Data.MySql/Statement/OracleStatement.cs
+++
@@ -1,6 +0,0 @@
-﻿using Zen.Pebble.Database;
-
-namespace Zen.Module.Data.MySql.Statement
-{
-    public class OracleStatement<T> : StatementRender<OracleStatementFragments, OracleWherePart, T> { }
-}
