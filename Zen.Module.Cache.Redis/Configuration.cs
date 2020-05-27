b9d4e7476f571f62376a2b02e963db4341a66bf6
Leo Botinelly <lmbs001@bucknell.edu>
2020-05-27 13:21:20 -0400

    Update Configuration.cs

--- Zen.Module.Cache.Redis/Configuration.cs
+++ Zen.Module.Cache.Redis/Configuration.cs
@@ -10,8 +10,15 @@ namespace Zen.Module.Cache.Redis
     {
         private readonly IOptions _options;
         public Configuration(IOptions<Options> options) => _options = options.Value;
-
         public void Configure(Options options) => options.CopyProperties(_options);
+
+
+        // Individual Configuration payload
+        public class ConnectionConfiguration
+        {
+            public string ConnectionString { get; set; }
+            public int DatabaseIndex { get; set; }
+        }
 
         public interface IOptions
         {
@@ -19,23 +26,21 @@ namespace Zen.Module.Cache.Redis
         }
 
         [IoCIgnore]
-        public class Options : IOptions {
+        public class Options : IOptions
+        {
             public Dictionary<string, ConnectionConfiguration> EnvironmentSettings { get; set; }
         }
 
         [Priority(Level = -99)]
-        public class AutoOptions : IOptions  // If nothing else is defined, AutoOptions kicks in.
+        public class AutoOptions : IOptions // If nothing else is defined, AutoOptions kicks in.
         {
-            public AutoOptions()
-            {
+            public AutoOptions() =>
                 EnvironmentSettings = new Dictionary<string, ConnectionConfiguration>
                 {
                     {"STA", new ConnectionConfiguration {DatabaseIndex = 5, ConnectionString = "localhost"}}
                 };
-            }
+
             public Dictionary<string, ConnectionConfiguration> EnvironmentSettings { get; set; }
         }
-
-
     }
 }
