b0c5a731438ca54941800cb6343e241bebd826e6
Leo Botinelly <lmbs001@bucknell.edu>
2020-05-27 13:21:03 -0400

    Create MySqlStatementFragments.cs

---
+++ Zen.Module.Data.MySql/Statement/MySqlStatementFragments.cs
@@ -0,0 +1,9 @@
+﻿using Zen.Pebble.Database.Renders.Relational;
+
+namespace Zen.Module.Data.MySql.Statement
+{
+    public class MySqlStatementFragments : RelationalStatementFragments
+    {
+        public override string ParametrizedValue => ":p{0}";
+    }
+}
