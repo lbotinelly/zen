6bd3b7fbf7784f7e8f91af1cff8675e12059c067
Leo Botinelly <lmbs001@bucknell.edu>
2020-06-04 16:25:16 -0400

    Sample User page

---
+++ Samples/Sample04-AuthenticatedWeb/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample04_AuthenticatedWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Update() => throw new NotImplementedException();
+
+        public IActionResult Delete() => throw new NotImplementedException();
+
+        public IActionResult Get() => throw new NotImplementedException();
+
+        public IActionResult Add() => throw new NotImplementedException();
+    }
+}
