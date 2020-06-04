8ea2532d433f209dd8efd3f6a7f0b45f76af3a99
Leo Botinelly <lmbs001@bucknell.edu>
2020-06-04 16:24:44 -0400

    Update StatementMasks.cs

--- Zen.Pebble.Database/Common/StatementMasks.cs
+++ Zen.Pebble.Database/Common/StatementMasks.cs
@@ -1,4 +1,7 @@
-﻿namespace Zen.Pebble.Database.Common
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zen.Pebble.Database.Common
 {
     public class StatementMasks
     {
@@ -6,19 +9,38 @@
         public string InlineParameter { get; set; } = "{0}";
         public string Column { get; set; } = "[{0}]";
 
-        public KeywordSet Keywords { get; set; } = new KeywordSet();
+        public MarkerSet Markers { get; set; } = new MarkerSet();
         public ValueSet Values { get; set; } = new ValueSet();
+        public string EnumType { get; set; }
+        public int MaximumTextSize { get; set; }
+        public string TextOverflowType { get; set; }
+        public char FieldDelimiter { get; set; }
 
-        public class KeywordSet
+        public class MarkerSet
         {
-            public string Keyset = "keyset";
+            public string KeySet = "keyset";
             public string Equality = "=";
+            public char Spacer { get; set; } = '_';
         }
 
         public class ValueSet
         {
             public object True { get; set; } = true;
             public object False { get; set; } = false;
+        }
+
+        public Dictionary<Type, TypeMapEntry> TypeMap = new Dictionary<Type, TypeMapEntry>();
+
+        public class TypeMapEntry
+        {
+            public string Name;
+            public string DefaultValue;
+
+            public TypeMapEntry(string name, string defaultValue = null)
+            {
+                Name = name;
+                DefaultValue = defaultValue;
+            }
         }
     }
 }
