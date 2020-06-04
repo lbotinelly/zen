2c6698a9a71cb347e96742e5513697a89e00a2bc
Leo Botinelly <lmbs001@bucknell.edu>
2020-06-04 16:24:22 -0400

    Create MySqlDefaultBundle.cs

---
+++ Zen.Module.Data.MySql/MySqlDefaultBundle.cs
@@ -0,0 +1,15 @@
+﻿using Zen.Base.Common;
+using Zen.Base.Module.Data.Connection;
+
+namespace Zen.Module.Data.MySql
+{
+    public class Default
+    {
+
+        [Priority(Level = -2)]
+        public class MySqlDefaultBundle : ConnectionBundlePrimitive
+        {
+            public MySqlDefaultBundle() => AdapterType = typeof(MySqlAdapter<>);
+        }
+    }
+}   
