c58d9160e2ab89c2ea029ff9e630a89c59bc6eb7
Leo Botinelly <lmbs001@bucknell.edu>
2020-06-04 16:24:47 -0400

    Delete ValueSet.cs

--- Zen.Pebble.Database/Common/ValueSet.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zen.Pebble.Database.Common {
-    public class ValueSet
-    {
-        public object True { get; set; } = true;
-        public object False { get; set; } = false;
-    }
-}
