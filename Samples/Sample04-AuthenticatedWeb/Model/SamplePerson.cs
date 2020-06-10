99d344015c8d144eaebe1e57ff3af8c6f2b750ba
Leo Botinelly <lmbs001@bucknell.edu>
2020-06-10 17:01:53 -0400

    Create SamplePerson.cs

---
+++ Samples/Sample04-AuthenticatedWeb/Model/SamplePerson.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Bogus;
+using Bogus.DataSets;
+using Zen.Base.Module;
+
+namespace Sample04_AuthenticatedWeb.Model
+{
+    public class SamplePerson : Data<SamplePerson>
+    {
+        [Key] public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Display] public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public Name.Gender? Gender { get; set; }
+
+        public static List<SamplePerson> Generate(int count = 100)
+        {
+            var res = new List<SamplePerson>();
+
+            var testSamplePerson = new Faker<SamplePerson>()
+                    .RuleFor(o => o.FirstName, (f, u) => f.Name.FirstName())
+                    .RuleFor(o => o.LastName, (f, u) => f.Name.LastName())
+                    .RuleFor(o => o.Email, (f, u) => f.Internet.Email())
+                ;
+
+            for (var i = 0; i < count; i++) res.Add(testSamplePerson.Generate());
+
+            return res;
+        }
+    }
+}
