ac8e7a991dc801f3b06fb3dc6d02a1cbe9edf138
Leo M Botinelly <lmbs001@bucknell.edu>
2020-09-14 14:55:58 -0400

    GraphQL (cont.)
    Initial loader and type mapper.

---
+++ Zen.Web.GraphQL/Attribute/GraphQlAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zen.Web.GraphQL.Attribute
+{
+    public class GraphQlAttribute : System.Attribute
+    {
+        public string Alias { get; set; }
+        public string SetName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Zen.Web.GraphQL/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using Zen.Base.Common;
+using Zen.Base.Module.Service;
+
+namespace Zen.Web.GraphQL
+{
+    public class Configuration : IConfigureOptions<Configuration.Options>
+    {
+        public enum ETypeNameResolution
+        {
+            Name,
+            FullName
+        }
+
+        private readonly IOptions _options;
+
+        public Configuration(IOptions<Options> options)
+        {
+            _options = options.Value;
+        }
+
+        public void Configure(Options options)
+        {
+            options.TypeNameResolution = _options.TypeNameResolution;
+        }
+
+        public interface IOptions
+        {
+            ETypeNameResolution TypeNameResolution { get; set; }
+        }
+
+        [IoCIgnore]
+        public class Options : IOptions
+        {
+            public ETypeNameResolution TypeNameResolution { get; set; }
+        }
+
+        [Priority(Level = -99)]
+        public class AutoOptions : IOptions // If nothing else is defined, AutoOptions kicks in.
+        {
+            public ETypeNameResolution TypeNameResolution { get; set; } = ETypeNameResolution.Name;
+        }
+    }
+}
