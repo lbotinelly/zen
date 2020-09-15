9b2af2a66f3089beaa190ac5de18c1cd5e5728e9
Leo M Botinelly <lmbs001@bucknell.edu>
2020-09-15 13:38:56 -0400

    LINQ - Almost there I think!

---
+++ Zen.Base/Module/Data/LINQ/DataContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Zen.Base.Module.Data.LINQ
+{
+    public class DataContext<T> : IOrderedQueryable<T> where T : Data<T>
+    {
+        public DataContext()
+        {
+            //System.Console.WriteLine("FileSystemContext()");
+            Provider = new DataProvider<T>();
+            Expression = Expression.Constant(this);
+        }
+
+        internal DataContext(IQueryProvider provider, Expression expression)
+        {
+            Provider = provider;
+            Expression = expression;
+        }
+
+        /// <summary>
+        ///     Return a type-safe Enumerator.
+        ///     <remarks>Unfortunately framework wants a non-generic Enumerator.</remarks>
+        /// </summary>
+        /// <returns>IEnumerator</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            //System.Console.WriteLine("GetEnumerator(1)");
+            return Provider.Execute<IEnumerable<T>>(Expression).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //System.Console.WriteLine("GetEnumerator(2)");
+            // call the generic version of the method
+            return GetEnumerator();
+        }
+
+        public Type ElementType => typeof(T);
+
+        public Expression Expression { get; }
+        public IQueryProvider Provider { get; }
+    }
+}
---
+++ Zen.Base/Module/Data/LINQ/DataQueryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Zen.Base.Module.Data.LINQ
+{
+    public class DataQueryContext<T> where T:Data<T>
+    {
+        internal static object Execute(Expression expression, bool isEnumerable)
+        {
+
+            var p1 = Expression.Parameter(typeof(T));
+            var expressionLambda = Expression.Lambda<Func<T, bool>>(expression, p1);
+
+            return Data<T>.Where(expressionLambda);
+        }
+     }
+
+
+}
