b11892758dc805bac7d97b2143aae291871a158a
Leo M Botinelly <lmbs001@bucknell.edu>
2020-10-06 15:26:53 -0400

    Update DataQueryContext.cs

--- Zen.Base/Module/Data/LINQ/DataQueryContext.cs
+++ Zen.Base/Module/Data/LINQ/DataQueryContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 
 namespace Zen.Base.Module.Data.LINQ
 {
@@ -8,35 +7,6 @@ namespace Zen.Base.Module.Data.LINQ
         internal static object Execute(Expression expression, bool isEnumerable)
         {
             return Data<T>.Where(expression.AsLambda<T>());
-        }
-    }
-
-    public static class Utils
-    {
-        public static Expression<Func<T, bool>> AsLambda<T>(this Expression expression)
-        {
-            // Strict call on Typecast
-            switch (expression)
-            {
-                case MethodCallExpression methodCallExpression:
-                    return methodCallExpression.AsLambda<T>();
-                case UnaryExpression unaryExpression:
-                    return unaryExpression.AsLambda<T>();
-                default:
-                    return null;
-            }
-        }
-
-        public static Expression<Func<T, bool>> AsLambda<T>(this MethodCallExpression expression)
-        {
-            // Extract the unary expression from the arguments
-            var unaryExpression = (UnaryExpression) expression.Arguments[1];
-            return unaryExpression.AsLambda<T>();
-        }
-
-        public static Expression<Func<T, bool>> AsLambda<T>(this UnaryExpression expression)
-        {
-            return (Expression<Func<T, bool>>) expression.Operand;
         }
     }
 }
