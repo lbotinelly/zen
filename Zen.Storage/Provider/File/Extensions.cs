19f6fd9360c3123445d447f5ab9d8fc09523f8ef
Leo M Botinelly <lmbs001@bucknell.edu>
2020-10-15 15:31:44 -0400

    Added GetDynamic

--- Zen.Storage/Provider/File/Extensions.cs
+++ Zen.Storage/Provider/File/Extensions.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Zen.Base.Extension;
 
 namespace Zen.Storage.Provider.File
@@ -10,6 +13,19 @@ namespace Zen.Storage.Provider.File
         {
             var payload = await repo.GetStream(path, file);
             return payload.XmlToType<T>();
+        }
+
+        public static async Task<dynamic> GetDynamic(this IFileStorage repo, string path, string file)
+        {
+            var content = await repo.GetText(path, file);
+
+            var doc = new XmlDocument();
+            doc.LoadXml(content);
+
+            var baseObj = JObject.Parse(JsonConvert.SerializeXmlNode(doc));
+
+            dynamic result = baseObj;
+            return result;
         }
 
         public static async Task<string> GetText(this IFileStorage repo, string path, string file)
