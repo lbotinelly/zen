80b5f79cc5abba13e90a4513e8ffcf5b4928c0f4
Leo M Botinelly <lmbs001@bucknell.edu>
2020-10-20 16:13:43 -0400

    ISetSave

---
+++ Zen.Base/Module/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Zen.Base.Module
+{
+    public static class Extensions
+    {
+        public static Set<T> ToSet<T>(this IEnumerable<T> source) where T : Data<T>, ISetSave
+        {
+            return new Set<T> {Cache = source.ToDictionary(i => i.GetDataKey(), i => i)};
+        }
+    }
+}
