37a18f3a0f5ca7824a043c9d27e70eb0d6caafa1
Leo M Botinelly <lmbs001@bucknell.edu>
2020-10-20 16:13:51 -0400

    Update ChangeTracker.cs

--- Zen.Pebble.CrossModelMap/Change/ChangeTracker.cs
+++ Zen.Pebble.CrossModelMap/Change/ChangeTracker.cs
@@ -6,28 +6,59 @@ using Zen.Base.Module;
 
 namespace Zen.Pebble.CrossModelMap.Change
 {
-    public class ChangeTracker<T>
+    public class ChangeTracker<T, TU> where TU : Data<TU>
     {
-        public Func<T, string> IdentifierMethod { get; private set; }
+        public MultiSet DataSets { get; } = new MultiSet();
 
-        public Func<T, string> ChecksumFunction { get; } = arg => arg.ToJson().Sha512Hash();
+        public Func<T, string> IdentifierFunc { get; private set; }
+
+        public Func<T, string> ChecksumFunc { get; } = arg => arg.ToJson().Sha512Hash();
+
+        public Func<T, string, string> SourceValueHandlerFunc { get; set; }
 
         public ChangeTrackerConfiguration Configuration { get; set; } = new ChangeTrackerConfiguration();
 
         public Dictionary<string, ChangeEntry<T>> Changes { get; private set; }
 
-        public ChangeTracker<T> Identifier(Func<T, string> function)
+        public Func<T, TU> ResolveTargetModelFunc { get; set; } = arg => null;
+
+        public ChangeTracker<T, TU> ContentTypeHandler(Action<string, string, object, ContentTypeHandlerResult> action)
         {
-            IdentifierMethod = function;
+            ContentTypeHandlerAction = action;
             return this;
         }
 
-        public string GetIdentifier(T model)
+        public Action<string, string, object, ContentTypeHandlerResult> ContentTypeHandlerAction { get; set; }
+        
+        public ChangeTracker<T, TU> SourceValueHandler(Func<T, string, string> function)
         {
-            return IdentifierMethod(model);
+            SourceValueHandlerFunc = function;
+            return this;
         }
 
-        public Dictionary<string, ChangeEntry<T>> DetectChanges(IEnumerable<T> modelCollection)
+        public ChangeTracker<T, TU> ResolveTargetModel(Func<T, TU> function)
+        {
+            ResolveTargetModelFunc = function;
+            return this;
+        }
+
+        public ChangeTracker<T, TU> Identifier(Func<T, string> function)
+        {
+            IdentifierFunc = function;
+            return this;
+        }
+
+        public virtual void Process()
+        {
+        }
+
+
+        public string GetIdentifier(T model)
+        {
+            return IdentifierFunc(model);
+        }
+
+        public Dictionary<string, ChangeEntry<T>> GetChanges(IEnumerable<T> modelCollection)
         {
             var sourceSetMap = modelCollection.ToList().ToDictionary(GetIdentifier, i => i);
 
@@ -43,7 +74,7 @@ namespace Zen.Pebble.CrossModelMap.Change
 
 
             foreach (var recordedChange in currentEntries.Where(recordedChange =>
-                recordedChange.Value.Checksum != ChecksumFunction(sourceSetMap[recordedChange.Key])))
+                recordedChange.Value.Checksum != ChecksumFunc(sourceSetMap[recordedChange.Key])))
             {
                 outputSet[recordedChange.Key] = recordedChange.Value;
                 outputSet[recordedChange.Key].Type = ChangeEntry<T>.EType.Update;
@@ -58,7 +89,7 @@ namespace Zen.Pebble.CrossModelMap.Change
 
                 outputSet[newEntry] = new ChangeEntry<T>
                 {
-                    Checksum = ChecksumFunction(targetModel),
+                    Checksum = ChecksumFunc(targetModel),
                     Model = targetModel,
                     Type = ChangeEntry<T>.EType.New,
                     Id = newEntry
@@ -70,25 +101,29 @@ namespace Zen.Pebble.CrossModelMap.Change
             return outputSet;
         }
 
-        public ChangeTracker<T> Configure(Action<ChangeTrackerConfiguration> action)
+        public ChangeTracker<T, TU> Configure(Action<ChangeTrackerConfiguration> action)
         {
             action.Invoke(Configuration);
             return this;
         }
 
-        public void ProcessChanges<TU>(Func<ChangeTracker<T>, T, TU> function) where TU : Data<TU>
+        public void ProcessChanges(Func<ChangeTracker<T, TU>, T, TU> function)
         {
             ProcessChanges(Changes, function);
         }
 
-        public void ProcessChanges<TU>(Dictionary<string, ChangeEntry<T>> changes,
-            Func<ChangeTracker<T>, T, TU> function)
-            where TU : Data<TU>
+        public void ProcessChanges(Dictionary<string, ChangeEntry<T>> changes,
+            Func<ChangeTracker<T, TU>, T, TU> function)
         {
             foreach (var (key, value) in changes)
                 try
                 {
+                    // First resolve the target record.
+                    var model = ResolveTargetModelFunc(value.Model);
+
                     // First use the simple Map iteration.
+
+                    ApplyValuesByMap(model, key, value);
 
 
                     //Then, if present, use the function.
@@ -106,11 +141,37 @@ namespace Zen.Pebble.CrossModelMap.Change
                 }
         }
 
+
+        private TU ApplyValuesByMap(TU model, string key, ChangeEntry<T> value)
+        {
+            foreach (var (k, v) in Configuration.MemberMapping.KeyMaps)
+            {
+                var sourceValue = SourceValueHandlerFunc(value.Model, k);
+                var destinationValue = model.GetMemberValue(v.Target);
+
+                var result = new ContentTypeHandlerResult();
+
+                ContentTypeHandlerAction.Invoke(v.Handler, sourceValue, destinationValue, result);
+
+                if (result.Success) model.SetMemberValue(v.Target, result.Value);
+            }
+
+            return model;
+        }
+
+        public class ContentTypeHandlerResult
+        {
+            public bool Success = false;
+            public object Value { get; set; }
+        }
+
         public class ChangeTrackerConfiguration
         {
             public TimeSpan? StaleRecordTimespan { get; set; } = TimeSpan.MaxValue;
             public string Set { get; set; }
             public string Collection { get; set; }
+            public MapDefinition MemberMapping { get; set; } = null;
+            public string SourceIdentifierPath { get; set; }
         }
     }
 }
