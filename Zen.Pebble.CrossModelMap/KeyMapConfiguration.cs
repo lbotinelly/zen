df178626a7c28a939d46546e7108ff6dbad90cf7
Leo M Botinelly <lmbs001@bucknell.edu>
2020-10-21 16:05:14 -0400

    Added alternate sources

--- Zen.Pebble.CrossModelMap/KeyMapConfiguration.cs
+++ Zen.Pebble.CrossModelMap/KeyMapConfiguration.cs
@@ -1,9 +1,11 @@
-﻿namespace Zen.Pebble.CrossModelMap
+﻿using System.Collections.Generic;
+
+namespace Zen.Pebble.CrossModelMap
 {
     public class KeyMapConfiguration
     {
         public string Target { get; set; }
         public string Handler { get; set; } = "text";
-        public string RootContext { get; set; }
+        public List<string> AternateSources { get; set; }
     }
 }
