d179856efd490283d29a55e2761cdc8e6136e898
Leo M Botinelly <lmbs001@bucknell.edu>
2020-10-22 15:12:44 -0400

    Pristine/Dirty tracking

--- Zen.Base/Module/Set.cs
+++ Zen.Base/Module/Set.cs
@@ -8,20 +8,15 @@ namespace Zen.Base.Module
     public sealed class Set<T> : ISetSave where T : Data<T>
     {
         internal Dictionary<string, T> Cache = new Dictionary<string, T>();
+        private readonly Dictionary<string, string> _checkSum = new Dictionary<string, string>();
 
         #region Overrides of Object
 
-        public override string ToString()
-        {
-            return $"{typeof(T).Name}: {Cache.Count} items";
-        }
+        public override string ToString() => $"{typeof(T).Name}: {Cache.Count} items";
 
         #endregion
 
-        public void Commit()
-        {
-            Save();
-        }
+        public void Commit() => Save();
 
         public T Fetch(string identifier, bool ignoreCache = false)
         {
@@ -42,16 +37,33 @@ namespace Zen.Base.Module
                 model = typeof(T).CreateInstance<T>();
                 if (identifier != null) model.SetDataKey(identifier);
             }
+            else
+            {
+                // Store the checksum to avoid commiting pristine copies.
+                _checkSum[model.GetDataKey()] = model.ToJson().Md5Hash();
+            }
 
-            Cache[model.GetDataKey()] = model;
+            Store(model);
             return model;
+        }
+
+        public void Store(T model)
+        {
+            Cache[model.GetDataKey()] = model;
         }
 
         public List<T> Save()
         {
-            var tempSet = Cache.Values.ToList();
-            tempSet.Save();
-            return tempSet;
+            var newModels = Cache.Where(i => !_checkSum.ContainsKey(i.Key)).Select(i=> i.Value).ToList();
+            var dirtyModels = Cache.Where(i => _checkSum.ContainsKey(i.Key) && _checkSum[i.Key] != i.Value.ToJson().Md5Hash()).Select(i=> i.Value).ToList();
+
+            var allChanges = new List<T>();
+
+            allChanges.AddRange(newModels);
+            allChanges.AddRange(dirtyModels);
+
+            allChanges.Save();
+            return allChanges;
         }
     }
 }
