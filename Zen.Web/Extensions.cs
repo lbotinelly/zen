a2717bdd889da196a9ed8031688343f0fc7a3d13
Leo Botinelly <lmbs001@bucknell.edu>
2020-11-10 08:34:26 -0500

    ToZenFile(this IFormFile formFile)

---
+++ Zen.Web/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Zen.Base.Extension;
+using Zen.Storage.Model;
+
+namespace Zen.Web
+{
+    public static class Extensions
+    {
+        public static ZenFile ToZenFile(this IFormFile formFile)
+        {
+            var res = new ZenFile();
+
+            var stream = formFile.OpenReadStream();
+
+            res.FileSize = formFile.Length;
+            res.Id = stream.HashGuid();
+            res.Locator = res.Id;
+            res.OriginalName = formFile.Name;
+            res.StorageName = res.Id + "-" + formFile.FileName.ToFriendlyUrl() + Path.GetExtension(formFile.FileName);
+            res.MimeType = formFile.ContentType;
+            res.Creation = DateTime.Now;
+            stream.Position = 0;
+            res.Store(stream).Wait();
+            res.Save();
+
+            return res;
+        }
+    }
+}
