e37b81ba38b006583196466440ee3246133d23fa
Leo Botinelly <lmbs001@bucknell.edu>
2020-11-10 08:34:59 -0500

    Update MediaExternalController.cs

--- Zen.Web/Media/MediaExternalController.cs
+++ Zen.Web/Media/MediaExternalController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Zen.Base;
 using Zen.Base.Extension;
@@ -11,13 +9,14 @@ namespace Zen.Web.Media
     [Route("api/media/external")]
     public class MediaExternalController : ControllerBase
     {
-        [HttpGet("get"), ResponseCache(Duration = 1 * 24 * 60 * 60, Location = ResponseCacheLocation.Any, NoStore = false)]
+        [HttpGet("get")]
+        [ResponseCache(Duration = 1 * 24 * 60 * 60, Location = ResponseCacheLocation.Any, NoStore = false)]
         public object Get()
         {
             var query = Request.Query;
             if (!query.ContainsKey("url")) return null;
 
-            var dictQuery = query.ToDictionary(i => i.Key, i => i.Value).OrderBy(i=> i.Key);
+            var dictQuery = query.ToDictionary(i => i.Key, i => i.Value).OrderBy(i => i.Key);
 
             var cacheTag = dictQuery.ToJson().Sha512Hash();
             var mimeCacheTag = cacheTag + "_mimeType";
@@ -25,7 +24,7 @@ namespace Zen.Web.Media
             var targetStream = Local.Read(cacheTag);
             var targetStreamMimeType = Local.ReadString(mimeCacheTag);
 
-            if (targetStream!= null && targetStreamMimeType!= null)
+            if (targetStream != null && targetStreamMimeType != null)
             {
                 Log.KeyValuePair(cacheTag + " " + targetStreamMimeType, query["url"]);
                 return File(targetStream, targetStreamMimeType);
