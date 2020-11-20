e44873db8e6d527f27b1e8a12913fbcd31e9bf5c
Leo Botinelly <leo.botinelly@bucknell.edu>
2020-11-20 15:32:45 -0500

    Create IVariantList.cs

---
+++ Zen.Pebble.FlexibleData/Common/Interface/IVariantList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Zen.Pebble.FlexibleData.Common.Interface
+{
+    public interface IVariantList<T> 
+    {
+        List<T> Variants { get; set; }
+    }
+}
