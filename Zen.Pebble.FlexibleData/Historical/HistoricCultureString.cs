e616ec65d8fc8532f0785df49e415c2542be1e1a
Leo Botinelly <leo.botinelly@bucknell.edu>
2020-11-20 15:33:40 -0500

    Update HistoricCultureString.cs

--- Zen.Pebble.FlexibleData/Historical/HistoricCultureString.cs
+++ Zen.Pebble.FlexibleData/Historical/HistoricCultureString.cs
@@ -4,7 +4,7 @@ using Zen.Pebble.FlexibleData.String.Localization.Interface;
 
 namespace Zen.Pebble.FlexibleData.Historical
 {
-    public class HistoricCultureString : Variant<TemporalCommented<string>>
+    public class HistoricCultureString : VariantList<TemporalCommented<string>>
     {
         public override string ToString() { return Variants.FirstOrDefault()?.Value; }
     }
