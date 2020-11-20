52737f8d525621c225be7ac2cd5b186723a982b9
Leo Botinelly <leo.botinelly@bucknell.edu>
2020-11-20 15:33:16 -0500

    Delete Variant.cs

--- Zen.Pebble.FlexibleData/Common/Interface/Variant.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Zen.Pebble.FlexibleData.Common.Interface {
-    public class Variant<T> : IVariant<T>
-    {
-        public List<T> Variants { get; set; }
-    }
-}
