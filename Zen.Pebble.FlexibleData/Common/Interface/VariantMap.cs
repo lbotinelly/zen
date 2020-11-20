d04e6e98436a4886646cc3e4e3e8fbadc7e47ec7
Leo Botinelly <leo.botinelly@bucknell.edu>
2020-11-20 15:33:28 -0500

    Create VariantMap.cs

---
+++ Zen.Pebble.FlexibleData/Common/Interface/VariantMap.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Zen.Pebble.FlexibleData.Common.Interface
+{
+    public class VariantMap<TU, T> : IVariantMap<TU, T>
+    {
+        public Dictionary<TU, VariantList<T>> Variants { get; set; }
+    }
+}
