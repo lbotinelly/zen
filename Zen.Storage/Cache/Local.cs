0161c74a372431752bbad7546ddcf0c3cba39383
Leo Botinelly <lmbs001@bucknell.edu>
2020-11-30 09:36:55 -0500

    Update Local.cs

--- Zen.Storage/Cache/Local.cs
+++ Zen.Storage/Cache/Local.cs
@@ -8,25 +8,24 @@ namespace Zen.Storage.Cache
     {
         public static readonly string BasePath = $"{Host.DataDirectory}{Path.DirectorySeparatorChar}cache{Path.DirectorySeparatorChar}";
 
-        public static string WriteString(string key, string source) { return Write(key, new MemoryStream(Encoding.Unicode.GetBytes(source))); }
+        public static string WriteString(string key, string source) { return Write(key, new MemoryStream(Encoding.UTF8.GetBytes(source))); }
 
         public static string ReadString(string key)
         {
-            var stream = Read(key);
+            using var stream = Read(key);
             return stream == null ? null : new StreamReader(stream).ReadToEnd();
         }
 
         public static string Write(string key, Stream source)
         {
             var targetFile = Path.Combine(BasePath, key);
-            var targetDirectory = Path.GetDirectoryName(targetFile);
+            Directory.CreateDirectory(Path.GetDirectoryName(targetFile));
 
-            Directory.CreateDirectory(Path.GetDirectoryName(targetDirectory));
-
-            var fileStream = File.Create(Path.Combine(BasePath, key));
+            using var fileStream = File.Create(targetFile);
             source.Seek(0, SeekOrigin.Begin);
             source.CopyTo(fileStream);
             fileStream.Close();
+
             source.Seek(0, SeekOrigin.Begin);
 
             return targetFile;
