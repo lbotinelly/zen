adf6dc9581839b730d8ce4f67997576708b42bc3
Leo Botinelly <lmbs001@bucknell.edu>
2020-11-30 09:37:07 -0500

    Create Settings.cs

---
+++ Zen.Web.App/Controllers/Settings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zen.Web.App.Properties;
+using Zen.Web.Common.Results;
+
+namespace Zen.Web.App.Controllers
+{
+    [Route("api/app/settings")]
+    public class Settings : ControllerBase
+    {
+        private static string _templateLoad;
+
+        [Route("{variableName}/js")]
+        [Route("{variableName}.js")]
+        [HttpGet]
+        public JavaScriptResult SettingsVariableJavaScript(string variableName) => JavaScriptSettingsPayload(variableName);
+
+        internal JavaScriptResult JavaScriptSettingsPayload(string variableName)
+        {
+            var preRet = variableName + " = " + App.Settings.InternalGetFrameworkSettings() + ";";
+
+            return new JavaScriptResult(preRet);
+        }
+
+
+        [Route("templateLoader.js")]
+        [HttpGet]
+        public JavaScriptResult GetTemplateInitialization()
+        {
+            if (_templateLoad == null) _templateLoad = Resources.template;
+
+            return new JavaScriptResult(_templateLoad);
+        }
+    }
+}
