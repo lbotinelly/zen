e6646acb5eb3b5f06982810d31f2ac2dc60766c3
Leo Botinelly <leo.botinelly@bucknell.edu>
2020-12-02 14:54:37 -0500

    CopyMembersTo

--- Zen.Base/Extension/Reflection.cs
+++ Zen.Base/Extension/Reflection.cs
@@ -391,6 +391,21 @@ namespace Zen.Base.Extension
             }
         }
 
+        public static void CopyMembersTo<T>(this T source, T target)
+        {
+            var type = typeof(T);
+            foreach (var sourceProperty in type.GetProperties())
+            {
+                var targetProperty = type.GetProperty(sourceProperty.Name);
+                targetProperty.SetValue(target, sourceProperty.GetValue(source, null), null);
+            }
+            foreach (var sourceField in type.GetFields())
+            {
+                var targetField = type.GetField(sourceField.Name);
+                targetField.SetValue(target, sourceField.GetValue(source));
+            }
+        }
+
         public static void CopyPropertiesTo<T, TU>(this T source, TU dest)
         {
             var sourceProps = typeof(T).GetProperties().Where(x => x.CanRead).ToList();
