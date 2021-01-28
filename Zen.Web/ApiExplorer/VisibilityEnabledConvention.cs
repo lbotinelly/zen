5e69de7047cc237723dcd75e65b569e8cd537272
Leo Botinelly <lmbs001@bucknell.edu>
2021-01-28 09:58:57 -0500

    Create VisibilityEnabledConvention.cs

---
+++ Zen.Web/ApiExplorer/VisibilityEnabledConvention.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Zen.Web.ApiExplorer
+{
+    public class VisibilityEnabledConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            foreach (var controller in application.Controllers)
+            {
+                if (controller.ApiExplorer.IsVisible == null)
+                {
+                    controller.ApiExplorer.IsVisible = true;
+                    controller.ApiExplorer.GroupName = controller.ControllerName;
+                }
+            }
+        }
+    }
+}
