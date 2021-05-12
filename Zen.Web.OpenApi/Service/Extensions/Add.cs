67dfd450038aaa4c75834d6ac883a956d30b3455
Leo Botinelly <leonardo.botinelly@gmail.com>
2021-05-12 09:50:09 -0400

    OpenApi

---
+++ Zen.Web.OpenApi/Service/Extensions/Add.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Zen.Base;
+
+namespace Zen.Web.OpenApi.Service.Extensions
+{
+    public static class Add
+    {
+        public static void AddOpenApi(this IServiceCollection services)
+        {
+            services.ResolveSettingsPackage();
+
+            services.Configure<Configuration.Options>(options =>
+                options.GetSettings<Configuration.IOptions, Configuration.Options>("OpenAPI"));
+        }
+    }
+}
