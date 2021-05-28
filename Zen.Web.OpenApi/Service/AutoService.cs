a2e091355cde1821ad6f15da755f55c3ac35301f
Leo Botinelly <leonardo.botinelly@gmail.com>
2021-05-28 15:13:34 -0400

    OpenAPI + cleanup

--- Zen.Web.OpenApi/Service/AutoService.cs
+++ Zen.Web.OpenApi/Service/AutoService.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Zen.Base;
 using Zen.Base.Common;
 using Zen.Base.Module.Service;
-using Zen.Web.OpenApi.Service.Extensions;
 
 namespace Zen.Web.OpenApi.Service
 {
     [Priority(Level = -97)]
-    public class AutoService : IZenAutoAddService
+    public class AutoService : IZenAutoAddService, IZenAutoUseService
     {
         public void Add(IServiceCollection services)
         {
-            services.AddOpenApi();
+            services.ResolveSettingsPackage();
+            services.AddOpenApiDocument();
+            services.Configure<Configuration.Options>(options => options.GetSettings<Configuration.IOptions, Configuration.Options>("OpenAPI"));
+        }
+
+        public void Use(IApplicationBuilder app, IHostEnvironment env = null)
+        {
+            app.UseOpenApi(); // Serves the registered OpenAPI/Swagger documents by default on `/swagger/{documentName}/swagger.json`
+            app.UseSwaggerUi3();
+            app.UseReDoc();
         }
     }
 }
