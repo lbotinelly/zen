973ecd096eaf1ec7dd1a46042fc9ee5e93401dde
Leo Botinelly <leonardo.botinelly@gmail.com>
2021-05-28 15:13:17 -0400

    OpenAPI documentation

--- Zen.Web/Diagnostics/DiagnosticsController.cs
+++ Zen.Web/Diagnostics/DiagnosticsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Zen.Web.Diagnostics
@@ -6,10 +7,20 @@ namespace Zen.Web.Diagnostics
     [Route("api/diagnostics")]
     public class DiagnosticsController : ControllerBase
     {
+        /// <summary>
+        /// The heartbeat endpoint verifies that the service is available.
+        /// </summary>
+        /// <response code="200">The service is available.</response>
         [HttpGet("heartbeat")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
         public JsonResult GetHeartbeat() { return new JsonResult("its alive!"); }
 
+        /// <summary>
+        /// Returns all host variables.
+        /// </summary>
+        /// <response code="200">Host variables compiled successfully.</response>
         [HttpGet("variables")]
-        public object GetVariables() { return Base.Host.Variables.ToDictionary(i=> i.Key, i=> i.Value.ToString()); }
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public object GetVariables() => Base.Host.Variables.ToDictionary(i => i.Key, i => i.Value?.ToString());
     }
 }
