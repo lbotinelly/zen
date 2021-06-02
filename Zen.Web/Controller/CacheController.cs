db0d88d5197bb1268ab2cea68249250a2faf80d3
Leo Botinelly <leonardo.botinelly@gmail.com>
2021-06-02 13:46:10 -0400

    Path refactoring, payload

---
+++ Zen.Web/Controller/CacheController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zen.Web.Controller
+{
+    /// <summary>
+    /// Provides cache maintenance methods.
+    /// </summary>
+    [Route("cache")]
+    public class CacheController : ControllerBase
+    {
+        /// <summary>
+        /// Clears all currently cached objects.
+        /// </summary>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("flush")]
+        public JsonResult FlushCache()
+        {
+            Base.Current.Cache.RemoveAll();
+            return new(new { message = "Cache flushed successfully." });
+        }
+    }
+}
