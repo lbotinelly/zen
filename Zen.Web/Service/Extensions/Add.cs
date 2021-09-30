81c0ee8327212501e76d8b4d7cc9c7efc0f6c49a
Leo Botinelly <leonardo.botinelly@gmail.com>
2021-09-30 11:29:52 -0400

    Move AddNewtonsoftJson to base Zen.Web module

--- Zen.Web/Service/Extensions/Add.cs
+++ Zen.Web/Service/Extensions/Add.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using Zen.Base;
 
 namespace Zen.Web.Service.Extensions
@@ -16,7 +19,20 @@ namespace Zen.Web.Service.Extensions
                 options.GetSettings<Configuration.IOptions, Configuration.IOptions>("Web");
             });
 
-            services.AddMvc(options => { });
+            services.AddMvc(options => { })
+                .AddNewtonsoftJson(options =>
+                {
+                    //Json serializer settings Enum as string, omit nulls.
+                    // https://gist.github.com/regisdiogo/27f62ef83a804668eb0d9d0f63989e3e
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
+                    // Return JSON responses in LowerCase?
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+
+                    // Resolve Looping navigation properties
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                }); ;
 
             var builder = new ZenWebBuilder(services);
 
