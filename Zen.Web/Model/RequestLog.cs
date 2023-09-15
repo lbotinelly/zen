c7e1ae99ca060301dcb2c05ac9c2d1e7da345783
Leon Aquitaine <leonaquitaine@gmail.com>
2023-09-15 19:14:50 -0400

    Create RequestLog.cs

---
+++ Zen.Web/Model/RequestLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Zen.Base.Module;
+using Zen.Base.Module.Data.CommonAttributes;
+
+namespace Zen.Web.Model
+{
+    public class RequestLog : Data<RequestLog>, IDataId
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Display]
+        public string Url { get; set; }
+
+        public string Referer { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
