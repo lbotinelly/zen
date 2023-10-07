a77e795125e5643096c5ed993aefc8a8d82eb241
Leon Aquitaine <leonaquitaine@gmail.com>
2023-10-07 01:33:49 -0400

    Dependency updates (#122)
    * Upsync (#2)
    
    * Development (#1) (#119)
    
    * IDataControllerPostFetchInterceptor + MemberExtract
    
    * Update Zen.Module.Data.SqlServer.csproj
    
    * Auth flow fix
    
    * Post-set handler
    
    * Add omnisearch capabilities (initial)
    
    * FIX - AND operator for Relational databases
    
    * Dependency update
    
    * Update zen.json
    
    * Added option to present enum properties in string format
    
    * cleanup
    
    * Default Health Check and x-correlation-id propagation
    
    * Update Sample02-REST.csproj
    
    * Simplified framework healthcheck
    
    * Allow for external tests.
    
    * Period update.
    
    * Metadata support
    
    * Development upsync (#113)
    
    * IDataControllerPostFetchInterceptor + MemberExtract
    
    * Update Zen.Module.Data.SqlServer.csproj
    
    * Auth flow fix
    
    * Post-set handler
    
    * Add omnisearch capabilities (initial)
    
    * FIX - AND operator for Relational databases
    
    * Dependency update
    
    * Update zen.json
    
    * Added option to present enum properties in string format
    
    * cleanup
    
    * Default Health Check and x-correlation-id propagation
    
    * Update Sample02-REST.csproj
    
    * Simplified framework healthcheck
    
    * Allow for external tests.
    
    * Period update.
    
    * Metadata support
    
    * Config consolidation
    
    * Flair
    
    * Support for base configuration and log filtering
    
    * Flair
    
    * Cleanup
    
    * FIX mutator propagation, added Web config options
    
    * FIX auth, small improvements
    
    * Added Discord provider
    
    ---------
    
    Co-authored-by: Leo Botinelly <leonardo.botinelly@gmail.com>
    
    * No idea.
    
    * Again.
    
    * 1
    
    * Dependency updates
    
    * Update OpenIdAuthEventHandler.cs
    
    * Update Configuration.cs
    
    ---------
    
    Co-authored-by: Leo Botinelly <leonardo.botinelly@gmail.com>

--- Zen.Base/Status.cs
+++ Zen.Base/Status.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.DependencyInjection;
 using Zen.Base.Common;
 using Zen.Base.Module.Service;
 
@@ -16,13 +15,9 @@ namespace Zen.Base
             ShuttingDown
         }
         public static EState State { get; internal set; }
-
         internal static void SetState(EState newState) { State = newState; }
-
         public static string InstanceId = Guid.NewGuid().ToString();
-
-        public static List<Type> Providers = new List<Type>();
-
+        public static List<Type> Providers = new();
         public static IZenProvider Service<T>(this T source) where T : Type => (IZenProvider)Instances.ServiceProvider.GetService(source);
     }
 }
