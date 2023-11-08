bdc167126811539a5407acbfb0959fc964008b76
Leon Aquitaine <leonaquitaine@gmail.com>
2023-11-08 10:20:40 -0500

    Update Program.cs

--- Samples/Sample05-Docker/Program.cs
+++ Samples/Sample05-Docker/Program.cs
@@ -1,4 +1,5 @@
 using MongoDB.Bson;
+using System.Collections.Generic;
 using Zen.MessageQueue.Shared;
 using Zen.Web.Host;
 
@@ -8,12 +9,17 @@ namespace Sample05_Docker
     {
         public static void Main(string[] args)
         {
+
             Zen.MessageQueue.Queue.RegisterType("string", true);
+            Zen.MessageQueue.Queue.RegisterType(new List<string>(), true);
+
             Zen.MessageQueue.Queue.Receive += (model) =>
             {
                 Zen.Base.Log.Add(model.ToJson());
             };
+
             Zen.MessageQueue.Queue.Send("string", EDistributionStyle.Broadcast);
+            Zen.MessageQueue.Queue.Send(new List<string> { "a", "b" }, EDistributionStyle.Broadcast);
 
             Builder.Start<Startup>(args);
 
