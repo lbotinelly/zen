2319a835bf1fe415de7bb186dfda81854f0d18b3
Leon Aquitaine <leonaquitaine@gmail.com>
2023-11-10 09:36:57 -0500

    Update Local.cs

--- Zen.Storage/Cache/Local.cs
+++ Zen.Storage/Cache/Local.cs
@@ -6,7 +6,13 @@ namespace Zen.Storage.Cache
 {
     public static class Local
     {
-        public static readonly string BasePath = Base.Current.Options?.CachePath ?? $"{Host.DataDirectory}{Path.DirectorySeparatorChar}cache{Path.DirectorySeparatorChar}";
+        public static readonly string BasePath;
+
+        static Local()
+        {
+            BasePath = Base.Current.Options?.CachePath ?? Path.Combine(Host.DataDirectory, "cache");
+            Base.Current.Log.KeyValuePair("Local Cache BasePath", BasePath, Base.Module.Log.Message.EContentType.StartupSequence);
+        }
 
         public static string WriteString(string key, string source) { return Write(key, new MemoryStream(Encoding.UTF8.GetBytes(source))); }
 
